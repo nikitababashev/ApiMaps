7dac9bf1636c23359b977350e6e6dda68b424b1b
RomaKozh <VAPOREON949@gmail.com>
2023-02-23 15:11:06 +0300

    second classwork

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtCore import Qt
 import os
 from mapd import Ui_Form
 
@@ -12,22 +13,20 @@ WIDTH, HEIGHT = 600, 500
 class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
+        self.delt = '0.005'
         self.setupUi(self)
         self.setWindowTitle('Карта')
         self.ready.clicked.connect(self.createmaps)
 
     def createmaps(self):
         coords = self.coords.text().split()
-        delta = self.delta.text()
-        print(coords, delta)
         map_api_server = 'http://static-maps.yandex.ru/1.x/'
         map_params = {
             'll': ','.join(coords),
-            'spn': ','.join([delta, delta]),
+            'spn': ','.join([self.delt, self.delt]),
             'l': 'map'
         }
         response = requests.get(map_api_server, params=map_params)
-        print(response)
 
         if not response:
             print('Код ошибки:', response.status_code)
@@ -38,13 +37,22 @@ class MyWidget(QMainWindow, Ui_Form):
             file.write(response.content)
 
         self.initUI()
+        self.delta.setText(f'Масштаб: {self.delt[:5]}')
 
     def initUI(self):
         pixmap = QPixmap(self.map_file)
-        self.label.setPixmap(pixmap)
+        self.map.setPixmap(pixmap)
 
     def closeEvent(self, event):
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and float(self.delt) > 0.005:
+            self.delt = str(float(self.delt) - 0.05)
+            self.createmaps()
+        if event.key() == Qt.Key_PageDown and float(self.delt) < 45:
+            self.delt = str(float(self.delt) + 0.05)
+            self.createmaps()
 
 
 app = QApplication(sys.argv)
